7d6cee05db06cace9afa90739b449c0724cc9cd8
Sean Doyle <sdoylelambda@gmail.com>
2020-11-30 13:11:18 -0500

    Boiler plate code removed, router added.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
+<script src="/path/to/vue.js"></script>
+<script src="/path/to/vue-router.js"></script>
+
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 export default {
   name: "App",
